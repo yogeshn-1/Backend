a731f39ebb5a701c787fd45945c6143ca0768fd4
yogeshn-1 <nirmalkary@gmail.com>
2024-04-25 13:30:08 +0530

    Configuring the express app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import dotenv from 'dotenv'
 import connectDB from './database/index.js'
+import { app } from './app.js'
 dotenv.config({ path: './env' })
-
+const port = process.env.PORT || 8000
 connectDB()
+    .then(() => {
+        console.log('DB Coonect success')
+        app.listen(port, () => {
+            console.log(`App is running on ${port}`)
+        })
+        app.on('error', (err) => console.log(err))
+    })
+    .catch((err) => console.log('Db error connection'))
